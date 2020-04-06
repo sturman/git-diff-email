0c141e1e6465255924e4b643c5cca1bb38e37728
Roman Isko <iskoroman@gmail.com>
2020-04-07 00:28:40 +0300

    Add more changes

--- diff.py
+++ diff.py
@@ -6,6 +6,7 @@ import requests
 with open(os.environ['GITHUB_EVENT_PATH']) as f:
 
     data = json.load(f)
+    print(data)
     commits = data['commits']
     for commit in commits:
         r = requests.get(commit['url'])
