71be79a2a92a9789e817cfd2de1205f0d787d842
Roman Isko <iskoroman@gmail.com>
2020-04-07 00:19:08 +0300

    Test tun python script with request diffs

--- diff.py
+++ diff.py
@@ -1,7 +1,11 @@
 import json
 import os
 
+import requests
+
 with open(os.environ['GITHUB_EVENT_PATH']) as f:
     data = json.load(f)
-
-print(data)
+    commits = data['commits']
+    for commit in commits:
+        r = requests.get(commit['url'])
+        print(r.text)
